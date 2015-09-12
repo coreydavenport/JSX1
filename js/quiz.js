6c9d643a8ed6e8701754a345149b05ea140d6576
Corey Davenport <cairnelan@gmail.com>
2015-09-11 21:48:59 -0400

    else
    else

--- js/quiz.js
+++ js/quiz.js
@@ -2,4 +2,6 @@ var answer = prompt('What programming language is the name of a certain snake?')
 if (answer === 'Python') {
 	document.write('<p>Right!</p>');
 
-};
+}else {
+	document.write('<p>WRONG!</p>');
+}
