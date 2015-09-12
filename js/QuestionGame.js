eebaa3311f8879ba2de136cc35e53447b353674b
Corey Davenport <cairnelan@gmail.com>
2015-09-12 00:27:07 -0400

    Question game
    question game

--- js/QuestionGame.js
+++ js/QuestionGame.js
@@ -4,3 +4,33 @@ var answer1 = prompt('What is javascript on the the serverside called?');
 if (answer1.toUpperCase() === 'NODE.JS'){
 	correct += 1;
 };
+
+var answer2 = prompt('What is jquery?');
+if (answer2.toUpperCase() === 'JAVASCRIPT LIBRARY'){
+	correct += 1;
+};
+
+var answer3 = prompt('What language uses media queries?');
+if (answer3.toUpperCase() === 'CSS'){
+	correct += 1;
+};
+
+var answer4 = prompt('What language is used for markup?');
+if (answer4.toUpperCase() === 'HTML'){
+	correct += 1;
+};
+
+var answer5 = prompt('True and False are examples of');
+if (answer5.toUpperCase() === 'BOOLEAN'){
+	correct += 1;
+};
+
+document.write('<p>You got ' + correct + ' questions out of 5 correct!</p>');
+
+if (correct == 5){
+	document.write('<p>You got gold!</p>');
+} else if (correct >= 3) {
+	document.write('<p>Yo got silver!</p>');
+}else{
+	document.write('<p>You got bronze!</p>');
+}
