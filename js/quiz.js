79edd3e4062f2b8ce6011ebb7520cc5bd1609553
Corey Davenport <cairnelan@gmail.com>
2015-09-11 22:53:50 -0400

    clearfix
    fixed

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 var answer = prompt('What programming language is the name of a certain snake?');
-if (answer === 'Python') {
+if (answer.toUpperCase === 'PYTHON') {
 	document.write('<p>Right!</p>');
 
 }else {
 	document.write('<p>WRONG!</p>');
-}
+};
