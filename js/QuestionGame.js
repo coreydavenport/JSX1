f87627d5e86f6bd2d28331173f50bd23f813f936
Corey Davenport <cairnelan@gmail.com>
2015-09-12 00:19:26 -0400

    question game exercise
    doing a question game js exercise

---
+++ js/QuestionGame.js
@@ -0,0 +1,6 @@
+var correct = 0;
+
+var answer1 = prompt('What is javascript on the the serverside called?');
+if (answer1.toUpperCase() === 'NODE.JS'){
+	correct += 1;
+};
--- js/RandomNumGame.js
+++ js/RandomNumGame.js
@@ -1,10 +1,24 @@
 // Random Number Guessing Game Exercise
 
+var correctGuess = false;
 var randomNum = Math.floor(Math.random() * 6) + 1;
 var guess = prompt('I\'m thinking of a number between 1 and 6. Can you guess?');
+if (parseInt(guess) === randomNum){
+	correctGuess = true;
+}else if ( parseInt(guess) < randomNum) {
+	var guessAgain = prompt('Guess higher. The number I\'m thinking of is greater than ' + guess);
+	if (parseInt(guessAgain) == randomNum) {
+		correctGuess = true;
+	}
+}else if ( parseInt(guess) > randomNum){
+	var guessLower = prompt('Guess lower. The number I\'m thinking of is less than ' + guess);
+	if (parseInt(guessLower) == randomNum) {
+		correctGuess = true;
+	}
 
-if (parseInt(guess) === randomNum) {
-	document.write('You\'re Correct!');
+}
+if ( correctGuess ) {
+	document.write('You\'re Correct! The number I was thinking of was ' + randomNum);
 } else {
 	document.write('WRONG! it was ' + randomNum);
 }
